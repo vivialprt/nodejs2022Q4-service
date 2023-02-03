f85861ec930471d2b78374eb138ad494177415d7
Ivan Hryhoryeu <Ivan.Hryhoryeu@itechart-group.com>
2023-02-03 12:11:37 +0200

    feat: read port from env file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+import { config} from 'dotenv';
+config();
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = Number(process.env.PORT) || 4000;
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
