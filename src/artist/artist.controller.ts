c642c91ef6df369bfeeeb5878141de91661405f3
Ivan Hryhoryeu <Ivan.Hryhoryeu@itechart-group.com>
2023-02-06 17:27:08 +0200

    docs: artist docs

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -9,30 +9,41 @@ import {
   ParseUUIDPipe,
   Put,
 } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiNoContentResponse, ApiNotFoundResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ArtistService } from './artist.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
 
 @Controller('artist')
+@ApiTags('Artist')
 export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
 
   @Post()
+  @ApiOperation({summary: 'Create artist.'})
+  @ApiBadRequestResponse({ description: 'Invalid request.'})
   async create(@Body() createArtistDto: CreateArtistDto) {
     return await this.artistService.create(createArtistDto);
   }
 
   @Get()
+  @ApiOperation({summary: 'Get all artists.'})
   async findAll() {
     return await this.artistService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({summary: 'Get artist by id.'})
+  @ApiBadRequestResponse({description: 'Invalid request.'})
+  @ApiNotFoundResponse({description: 'Artist not found.'})
   async findOne(@Param('id', ParseUUIDPipe) id: string) {
     return await this.artistService.findOne(id);
   }
 
   @Put(':id')
+  @ApiOperation({summary: 'Change artist.'})
+  @ApiBadRequestResponse({description: 'Invalid request.'})
+  @ApiNotFoundResponse({description: 'Artist not found.'})
   async update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateArtistDto: UpdateArtistDto,
@@ -42,6 +53,10 @@ export class ArtistController {
 
   @Delete(':id')
   @HttpCode(204)
+  @ApiOperation({ summary: 'Delete artist.' })
+  @ApiNoContentResponse({description: 'Artist deleted.'})
+  @ApiBadRequestResponse({description: 'Invalid request.'})
+  @ApiNotFoundResponse({description: 'Artist not found.'})
   async remove(@Param('id', ParseUUIDPipe) id: string) {
     return await this.artistService.remove(id);
   }
