eda274877a5b7ea166591e5e0b05e4773c7376d2
Ivan Hryhoryeu <Ivan.Hryhoryeu@itechart-group.com>
2023-02-06 16:55:07 +0200

    init: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,20 @@ config();
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   const PORT = Number(process.env.PORT) || 4000;
+  
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription('Home Library Service')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(PORT);
