9c382c341734169c57c391f76ccab11fa04e34d7
Ivan Hryhoryeu <ivan.grigoryev502@gmail.com>
2023-02-20 22:45:33 +0200

    feat: prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { FavouritesModule } from './favourites/favourites.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { FavouritesModule } from './favourites/favourites.module';
     TrackModule,
     AlbumModule,
     FavouritesModule,
+    PrismaModule
   ],
   controllers: [AppController],
   providers: [AppService],
